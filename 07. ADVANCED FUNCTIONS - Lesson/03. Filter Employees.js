d943e4cccc1b8243125c220162f245a0fdae891d
Boryana Lengerova <b.arizanova@abv.bg>
2020-06-11 22:27:35 +0300

    07. ADVANCED FUNCTIONS - Lesson

---
+++ 07. ADVANCED FUNCTIONS - Lesson/03. Filter Employees.js
@@ -0,0 +1,44 @@
+function filterEmployees(array, filterString){
+    let employees = JSON.parse(array);
+    let filterBy = filterString.split("-")[0];
+    let filterValue = filterString.split("-")[1];
+
+    let filterdedArray = employees.filter(e => e[filterBy] === filterValue);
+
+    let result = [];
+
+    for (let index = 0; index < filterdedArray.length; index++) {
+        const element = filterdedArray[index];
+        result.push(`${index}. ${element.first_name} ${element.last_name} - ${element.email}`);
+    }
+
+    return result.join("\n");
+}
+
+console.log(filterEmployees(`[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+ 'last_name-Johnson'
+));
